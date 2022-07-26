220f49d4830d22b8263598132c2ec603baa31e54
jasoncheng <shenjingwa444@qq.com>
2022-07-26 17:18:00 +0800

    完成å播放、暂停功能

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,10 +1,11 @@
 import "./icons"
-const $ = selector=>document.querySelector(selector)
+
+const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 
-class Player{
-  constructor(node){
-    this.root = typeof node === 'string' ? $(node) : node
+class Player {
+  constructor(node) {
+    this.root = typeof node === "string" ? $(node) : node
     this.songList = []
     this.currentIndex = 0
     this.audio = new Audio()
@@ -12,24 +13,35 @@ class Player{
     this.bind()
   }
 
-  start(){
-    fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json')
-      .then(res=>res.json())
-      .then(data=>{
+  start() {
+    fetch("https://jirengu.github.io/data-mock/huawei-music/music-list.json")
+      .then(res => res.json())
+      .then(data => {
         this.songList = data
+        this.audio.src = this.songList[this.currentIndex].url
       })
   }
 
-  bind(){
+  bind() {
     let self = this
-    this.root.querySelector('.btn-play-pause').onclick = function(){
-      self.playSong()
+    this.root.querySelector(".btn-play-pause").onclick = function () {
+      if (this.classList.contains("play")) {
+        self.audio.pause()
+        this.classList.remove("play")
+        this.classList.add("pause")
+        this.querySelector("use").setAttribute("xlink:href", "#icon-pause")
+      } else if (this.classList.contains("pause")) {
+        this.classList.remove("pause")
+        this.classList.add("play")
+        this.querySelector("use").setAttribute("xlink:href", "#icon-play")
+        self.audio.play()
+      }
     }
   }
-  playSong(){
-    this.audio.src = this.songList[this.currentIndex].url
+
+  playSong() {
     this.audio.play()
   }
 }
 
-new Player('#player')
+new Player("#player")
